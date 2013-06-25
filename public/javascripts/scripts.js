e3a9f6fe6c5519d66fd97db61ca5e7cc16357d27
Chad Bartels <chad.bartels@gmail.com>
2013-06-25 00:04:14 -0400

    added iCanHaz templating

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,15 +3,24 @@ $(function() {
     type:'POST',
     url: 'http://localhost:30000/cnnbrk-tweets'
   }).done(function(data) {
-    console.log(data);
+    var tweet_data;
+    
     $.each(data, function(k,v){
       var origText = tweets.replaceURL(v.text);
       var newText = tweets.replaceHash(origText);
-      console.log(k);
+      
+      tweet_data = {
+        text: newText,
+        name: v.user.screen_name,
+        created_at: v.created_at.substring(0,19)
+      }
+      
+      tweet = ich.tweet(tweet_data);
+      
       if(k <= 4) {
-        $(".tweets .column1").append('<div class="tweet"><p>' + newText + '</p><small>Tweeted By <a href="http://twitter.com/' + v.user.screen_name + '">' + v.user.screen_name + '</a> at <span>' + v.created_at.substring(0,19) + '</span></small></div>');
+        $(".tweets .column1").append(tweet);
       }else{
-        $(".tweets .column2").append('<div class="tweet"><p>' + newText + '</p><small>Tweeted By <a href="http://twitter.com/' + v.user.screen_name + '">' + v.user.screen_name + '</a> at <span>' + v.created_at.substring(0,19) + '</span></small></div>');
+        $(".tweets .column2").append(tweet);
       }
     });
   });
